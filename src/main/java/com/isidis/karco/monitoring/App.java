fd9382e89b0018305c04379a5692eecaab0ae634
Victor <victor.varada@gmail.com>
2017-06-30 09:29:09 +0200

    Initial commit

---
+++ src/main/java/com/isidis/karco/monitoring/App.java
@@ -0,0 +1,13 @@
+package com.isidis.karco.monitoring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
